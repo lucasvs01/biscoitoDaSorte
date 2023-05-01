edb6286409ef941908f63ef16e4575b6179ddb96
lucasvs001 <lucasvs04@gmail.com>
2023-05-01 18:03:20 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const menssageList = [
+    "A vida trará coisas boas se tiver paciência.",
+    "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+    "Não compense na ira o que lhe falta na razão.",
+    "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+    "A maior de todas as torres começa no solo.",
+    "Não há que ser forte. Há que ser flexível.",
+    "Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?",
+    "Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.",
+    "A juventude não é uma época da vida, é um estado de espírito.",
+    "Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.",
+]
+let radonNumber = Math.floor(Math.random()*10)
+let printMenssage = menssageList[radonNumber]
+
+
+function returnTextH2P () {
+    let h2 = document.querySelector(".screen2 p")
+    h2.innerHTML = printMenssage
+}
+
+returnTextH2P();
